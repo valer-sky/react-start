2401196bd1a6b975ec2ec41863e71dc922f9db2d
valer-sky <89993618+valer-sky@users.noreply.github.com>
2022-06-23 18:05:59 +0300

    23.06.2022

--- my-app/src/components/toDo/index.js
+++
@@ -1,1 +0,0 @@
-import ToDo from './ToDo'
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// const elem = <h2>Hello world!</h2>;
 
-const elem = React.createElement('h2', {className: 'greeting'}, 'Hello ')
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- elem
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+    
+ 
 );
